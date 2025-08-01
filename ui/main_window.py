e5bff2bf333bdd33ac40b91d8ebf75a88262738f
Ryuki <ryukifujita0530@gmail.com>
2025-08-01 10:09:04 +0900

    ショートカットを追加

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout,QWidget, QLineEdit, QPushButton, QSplitter, QTextEdit
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtCore import Qt, QUrl, QEvent
+from PyQt5.QtGui import QKeySequence
 from core.browser_controller import BrowserController
 """
 メインウィンドウのUI設計とレイアウト管理
@@ -65,6 +66,13 @@ class MainWindow(QMainWindow):
         container.setLayout(main_layout)
         self.setCentralWidget(container)
 
+        # ショートカットキー
+        self.shortcut_reload = QKeySequence("Ctrl+R")
+        self.shortcut_back = QKeySequence("Alt+A")
+        self.shortcut_forward = QKeySequence("Alt+D")
+        self.installEventFilter(self)
+        
+
 
     # 各種の操作メソッド
     def load_url(self):
@@ -79,3 +87,33 @@ class MainWindow(QMainWindow):
     
     def go_forward(self):
         self.controller.go_forward(self.browser_view)
+
+
+    # イベントフィルター
+    def eventFilter(self, source, event):
+        # キーボードショートカット処理
+        if event.type() == QEvent.KeyPress:
+            if event.matches(QKeySequence.Refresh) or (event.modifiers() == Qt.ControlModifier and event.key() == Qt.Key_R):
+                self.reload_page()
+                return True
+        
+            elif event.matches(QKeySequence.Back) or (event.modifiers() == Qt.AltModifier and event.key() == Qt.Key_A):
+                self.go_back()
+                return True
+            
+            elif event.matches(QKeySequence.Forward) or (event.modifiers() == Qt.AltModifier and event.key() == Qt.Key_D):
+                self.go_forward()
+                return True
+        
+        # マウスボタン処理
+        elif event.type() == QEvent.MouseButtonPress:
+            if event.button() == Qt.BackButton:  # マウスの戻るボタン
+                print("Back button pressed")
+                self.go_back()
+                return True
+            elif event.button() == Qt.ForwardButton:  # マウスの進むボタン
+                print("Forward button pressed")
+                self.go_forward()
+                return True
+        
+        return super().eventFilter(source, event)
